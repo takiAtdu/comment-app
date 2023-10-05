10ca3c25bb8c29a3236a6c5a61b014c3635015b3
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-10-05 21:10:08 +0900

    [add]基本的なログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "users#new"
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :texts
   resources :comments

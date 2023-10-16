896820094b50bbf4b062efee56d6f1c5b4718732
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-10-16 14:15:35 +0900

    [fix]wkhtmltopdfのパズ

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,6 +1,7 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf' # wkhtmltopdfのバイナリパスを指定
+  # config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf' # wkhtmltopdfのバイナリパスを指定
+  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
   config.default_options = {
     page_size: 'Letter', 
     quiet: false, 
